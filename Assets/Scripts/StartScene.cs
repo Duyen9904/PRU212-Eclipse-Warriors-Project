9a9846474c43acb600ffac21d7492d27b6f7d3a6
Rvt129 <baothuvoal@gmail.com>
2025-03-26 00:32:29 +0700

    Update start-game

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartScene : MonoBehaviour
+{
+    public void LoadGlacierScene()
+    {
+        SceneManager.LoadScene("GlacierBiome"); // Thay tên scene nếu cần
+    }
+}
